04621b286ba6a832a0ea06e3d0ca1a44f43192e9
Matvey Khanzhin <motyalove17@mail.ru>
2025-05-01 22:18:11 +0300

    Реализовал обработку исключений

---
+++ Utils.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdexcept>
+#include <limits>
+
+using namespace std;
+
+template <typename T>
+
+T GetIntValue(){
+    T n;
+    wcin.exceptions(wios::failbit | wios::badbit);  // failbit - невозможность интерпретирования string в int
+                                                    // badbit - ошибка чтения потока
+    while (true){
+        try{
+            wcin >> n;
+            wcin.ignore(numeric_limits<streamsize>::max(), L'\n');
+            return n;
+        }
+        catch (const ios_base::failure& error){
+            wcout << L"Неверный ввод, ошибка" << error.what() <<  L"! Повторите попытку: ";
+            wcin.clear();
+            wcin.ignore(numeric_limits<streamsize>::max(), L'\n');
+        }
+    }
+}
